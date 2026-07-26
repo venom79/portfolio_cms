6c4c1c970c18dcb5fec96ec150e22bd4cbdc7c6c
Aditya <aditya.gaonkar1907@gmail.com>
2026-07-26 18:18:24 +0530

    re design backend

--- admin/src/components/layout/MobileSidebar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MobileSidebar = () => {
-  return <div>MobileSidebar</div>;
-};
-
-export default MobileSidebar;
--- admin/src/pages/Milestones.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Milestones = () => {
-  return <div>Milestones</div>;
-};
-
-export default Milestones;
